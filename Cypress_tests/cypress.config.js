563c51af105259b7d1b4064522e157a715cf3d3b
Juan Montenegro <jj.montenegro@uniandes.edu.co>
2023-05-20 09:52:55 -0500

    update cypress config file

--- Cypress_tests/cypress.config.js
+++ Cypress_tests/cypress.config.js
@@ -6,4 +6,12 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  env: {
+    baseURL: "http://localhost:2368",
+    ghostV3: "http://localhost:2369",
+    credentials: {
+      email: "correoDePrueba@gmail.com",
+      password: "@1234567890*",
+    },
+  },
 });
