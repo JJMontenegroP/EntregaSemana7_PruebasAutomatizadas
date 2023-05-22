63d9bbef92d8ff4cde69b3765efc4e59359de7e5
lrozoq <l.rozoq@uniandes.edu.co>
2023-05-21 23:47:13 -0500

    cambios

--- Cypress_tests/cypress/e2e/v5.47.1/scenarios/poolPseudoAleatorioDinamico/6. tagsManagement_Functionality.cy.js
+++ Cypress_tests/cypress/e2e/v5.47.1/scenarios/poolPseudoAleatorioDinamico/6. tagsManagement_Functionality.cy.js
@@ -1,10 +1,10 @@
-import CreateTag from "../pages/CreateTag";
-import LoginPage from "../pages/LoginPage";
+import CreateTag from "../../pages/CreateTag";
+import LoginPage from "../../pages/LoginPage";
 
 const timestamp = Date.now();
 
 const CONSTANTS = {
-    TAG_TITLE: `Test tag title ${timestamp}`,
+    TAG_DATA_FILE: "../e2e/v5.47.1/scenarios/poolDataA-priori/data/6.tagsManagement_Functionality/mock_data_tags.json",
   };
 
 describe("Management tags on website", () => {
@@ -16,15 +16,26 @@ describe("Management tags on website", () => {
         LoginPage.clickFormLogin();
     });
     it("Create new tag", () => {
-        CreateTag.newTag(CONSTANTS.TAG_TITLE);
-        cy.contains(CONSTANTS.TAG_TITLE).eq(0).should("exist");
-        cy.screenshot(`v5-${Cypress.currentTest.titlePath.join("/")}/step`);
+        cy.request({
+            method: "GET",
+            url: "https://my.api.mockaroo.com/tags_api.json?key=04f62920",
+            responseType: "text"
+          }).then(tags => {
+            tags.forEach(tag=>{
+            CreateTag.newTag(tag.name, tag.color, tag.slug, tag.description);
+            cy.contains(tag.name).eq(0).should("exist");
+            cy.screenshot(`v5-${Cypress.currentTest.titlePath.join("/")}/step`);
+            });
+        });
     });
 
     it("Delete tag", () => {
-        CreateTag.newTag(CONSTANTS.TAG_TITLE);
-        CreateTag.deleteTag(CONSTANTS.TAG_TITLE);
-        cy.contains(CONSTANTS.TAG_TITLE).eq(0).should("exist");
-        cy.screenshot(`v5-${Cypress.currentTest.titlePath.join("/")}/step`);
+        cy.fixture(CONSTANTS.TAG_DATA_FILE).then(tags => {
+            tags.forEach(tag=>{
+            CreateTag.newTag(tag.name, tag.color, tag.slug, tag.description);
+            CreateTag.deleteTag(tag.name);
+            cy.contains(tag.name).eq(0).should("exist");
     });
+    });
+});
 });
