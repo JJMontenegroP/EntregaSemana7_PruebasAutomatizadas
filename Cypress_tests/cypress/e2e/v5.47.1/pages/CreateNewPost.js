6fe646ec0787effc6a4294c43364c702861597e8
Juan Montenegro <jj.montenegro@uniandes.edu.co>
2023-05-21 23:22:44 -0500

    CreatePost_Functionality with the tree data generation scenaries

--- Cypress_tests/cypress/e2e/v5.47.1/pages/CreateNewPost.js
+++ Cypress_tests/cypress/e2e/v5.47.1/pages/CreateNewPost.js
@@ -1,36 +1,60 @@
-class CreateNewPost{
+class CreateNewPost {
+  elements = {
+    newPostButton: () => cy.get("a").contains("New post"),
+    previewButton: () => cy.get("button[data-test-button='publish-preview']"),
+  };
 
-    writePost(post_title,post_content){
-        cy.get("a[data-test-nav-custom='posts-Published']").click();
-        cy.wait(2000);
-        cy.get("a[data-test-new-post-button]").click();
-        cy.wait(2000);
-        cy.get("textarea[data-test-editor-title-input]").type(post_title);
-        cy.get("div[data-kg=editor]").click().type(post_content);
-    }
+  requestsSuccess = {
+    getMockarooDataFromAPI: () => {
+      const API_KEY = "247935e0";
 
-    publishPost(){
-        cy.get("button[data-test-button='publish-preview']").click();
-        cy.wait(6000);
-        cy.get("button").contains("Publish").click({ force: true }); 
-        cy.get("button[data-test-button='continue']").click();
-        cy.get("button[data-test-button='confirm-publish']").click();
- 
-    }
+      return cy
+        .request(
+          `https://my.api.mockaroo.com/create_post_funtionality_Success.json?key=${API_KEY}`
+        )
+        .then((response) => response.body);
+    },
+  };
 
-    addImage(image){
-        cy.get("textarea[data-test-editor-title-input]").click()
-        cy.get("button").contains("span", "Add feature image").click();
-        cy.wait(1000);
-        cy.get("input[type=file]").invoke("show").selectFile(image, { force: true });
-        cy.wait(1000);
-    }
-    addImageUnplash(){
-        cy.get("textarea[data-test-editor-title-input]").click();
-        cy.wait(1000);
-        cy.get("svg[fill='currentColor']").click();
-        cy.get("a[class=gh-unsplash-button]").first().click();
-        cy.wait(1000);
-    }
+  requestsWrong = {
+    getMockarooWrongDataFromAPI: () => {
+      const API_KEY = "247935e0";
+
+      return cy
+        .request(
+          `https://my.api.mockaroo.com/create_post_funtionality_wrong.json?key=${API_KEY}`
+        )
+        .then((response) => response.body);
+    },
+  };
+
+  writePost(post_title, post_content) {
+    cy.get("textarea[data-test-editor-title-input]").type(post_title);
+    cy.get("div[data-kg=editor]").click().type(post_content);
+  }
+
+  publishPost() {
+    cy.get("button[data-test-button='publish-preview']").click();
+    cy.get("button").contains("Publish").click({ force: true });
+    cy.get("button[data-test-button='continue']").click();
+    cy.get("button[data-test-button='confirm-publish']").click();
+  }
+
+  addImage(image) {
+    cy.get("textarea[data-test-editor-title-input]").click();
+    cy.get("button").contains("span", "Add feature image").click();
+    cy.wait(1000);
+    cy.get("input[type=file]")
+      .invoke("show")
+      .selectFile(image, { force: true });
+    cy.wait(1000);
+  }
+  addImageUnplash() {
+    cy.get("textarea[data-test-editor-title-input]").click();
+    cy.wait(1000);
+    cy.get("svg[fill='currentColor']").click();
+    cy.get("a[class=gh-unsplash-button]").first().click();
+    cy.wait(1000);
+  }
 }
 export default new CreateNewPost();
--- Cypress_tests/cypress/e2e/v5.47.1/scenarios/poolDataA-priori/2. createNewPost_Funcionality.cy.js
+++ Cypress_tests/cypress/e2e/v5.47.1/scenarios/poolDataA-priori/2. createNewPost_Funcionality.cy.js
@@ -1,92 +1,39 @@
 import LoginPage from "../../pages/LoginPage";
 import CreateNewPost from "../../pages/CreateNewPost";
+import mockDataCreatePostSuccess from "../poolDataA-priori/data/2. createpost_functionality/mock_createPost_fucntionality_1_success.json";
+import mockDataCreatePostWrong from "../poolDataA-priori/data/2. createpost_functionality/mock_createPost_fucntionality_2_wrong.json";
 
 const ghostUrl = Cypress.env("baseURL");
-const CONSTANTS = {
-  POST_TITLE: "Test post title",
-  POST_CONTENT: "Test post content",
-  IMAGE: "cypress/fixtures/logo.png",
-};
 
 describe("Create a ghost new Post Functionality", () => {
-  it("Write a post for the website created in Ghost only using text", () => {
-    // Given that I am logged into my Ghost account
+  beforeEach(() => {
     LoginPage.visitLoginPage();
     LoginPage.fillEmailLogin();
     LoginPage.fillPasswordLogin();
     LoginPage.clickFormLogin();
-    cy.visit(`${ghostUrl}/ghost/#/site`);
-    cy.wait(3000);
 
-    // When I write and publish a new post
-    CreateNewPost.writePost(CONSTANTS.POST_TITLE, CONSTANTS.POST_CONTENT);
-    loginPage;
-    CreateNewPost.publishPost();
-    loginPage;
-
-    // Then I should see the post published on the website
-    cy.contains(CONSTANTS.POST_TITLE);
-    cy.contains(CONSTANTS.POST_CONTENT);
+    cy.visit(`${ghostUrl}/ghost/#/posts`);
   });
 
-  it("Write a post for the website created by attaching an image from my device", () => {
-    // Given that I am logged into my Ghost account
-    LoginPage.visitLoginPage();
-    LoginPage.fillEmailLogin();
-    LoginPage.fillPasswordLogin();
-    LoginPage.clickFormLogin();
-    cy.wait(1000);
-    cy.visit(`${ghostUrl}/ghost/#/site`);
+  // Create a post with only text
+  mockDataCreatePostSuccess.forEach(({ Title, Content }) => {
+    it("Write a post for the website created in Ghost only using text", () => {
+      // Given that I am logged into my Ghost account
+      CreateNewPost.elements.newPostButton().click();
+      CreateNewPost.writePost(Title, Content);
 
-    // When I write and publish a new post with an image
-    CreateNewPost.writePost(CONSTANTS.POST_TITLE, CONSTANTS.POST_CONTENT);
-    loginPage;
-    CreateNewPost.addImage(CONSTANTS.IMAGE);
-    loginPage;
-    CreateNewPost.publishPost();
-    loginPage;
-
-    // Then I should see the post published on the website with the image attached
-    cy.contains(CONSTANTS.POST_TITLE);
-    cy.contains(CONSTANTS.POST_CONTENT);
-    loginPage;
+      CreateNewPost.publishPost();
+    });
   });
 
-  it("Write a post for the website created by attaching a selected image on the unsplash", () => {
-    // Given that I am logged into my Ghost account
-    LoginPage.visitLoginPage();
-    LoginPage.fillEmailLogin();
-    LoginPage.fillPasswordLogin();
-    LoginPage.clickFormLogin();
-    cy.visit(`${ghostUrl}/ghost/#/site`);
+  // Create a post with a naughty string in each
+  mockDataCreatePostWrong.forEach(({ Title, Content }) => {
+    it("Write a post for the website created in Ghost only using any type of characters", () => {
+      // Given that I am logged into my Ghost account
+      CreateNewPost.elements.newPostButton().click();
+      CreateNewPost.writePost(Title, Content);
 
-    // When I write and publish a new post with an image
-    CreateNewPost.writePost(CONSTANTS.POST_TITLE, CONSTANTS.POST_CONTENT);
-    CreateNewPost.addImageUnplash(CONSTANTS.IMAGE);
-    CreateNewPost.publishPost();
-
-    // Then I should see the post published on the website with the image attached
-    cy.contains(CONSTANTS.POST_TITLE);
-    cy.contains(CONSTANTS.POST_CONTENT);
-    loginPage;
-  });
-
-  it("Write a post with title (Untitled) when there is no input in title field", () => {
-    // Given that I am logged into my Ghost account
-    LoginPage.visitLoginPage();
-    LoginPage.fillEmailLogin();
-    LoginPage.fillPasswordLogin();
-    LoginPage.clickFormLogin();
-    cy.visit(`${ghostUrl}/ghost/#/site`);
-
-    // When I write and publish a new post with an image
-    CreateNewPost.writePost("(Untitled)", CONSTANTS.POST_CONTENT);
-    CreateNewPost.addImageUnplash(CONSTANTS.IMAGE);
-    CreateNewPost.publishPost();
-
-    // Then I should see the post published on the website with the image attached
-    cy.contains("(Untitled)");
-    cy.contains(CONSTANTS.POST_CONTENT);
-    loginPage;
+      CreateNewPost.elements.previewButton().should("not.exist");
+    });
   });
 });
